b55b7bdaa066f9ae9e0df8f475790325d8865841
Patryk Stanek <p.stanek3@gmail.com>
2019-05-14 18:26:08 +0200

    login page finished

--- server.js
+++ server.js
@@ -8,16 +8,27 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use((req, res, next) => {
-    console.log('app.use');
     next();
 });
 
 app.get('/', (req, res) => {
-    res.send('/main');
+    res.render('index', {
+        title: "/index"
+    });
 });
 
-app.get('/index', (req, res) => {
-    res.render('index');
+app.get('/auth/google', (req, res) => {
+    res.render('auth', {
+        title: "/auth"
+    });
+});
+
+app.get('/default', (req, res) => {
+    res.render('default', {
+        title: "/default",
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    });
 });
 
 app.listen(3000);
